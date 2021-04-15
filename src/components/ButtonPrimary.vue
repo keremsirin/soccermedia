d2cc001da52201822770d63e9954e1b2365995b7
Kerem Şirin <30510378+keremsirin@users.noreply.github.com>
2021-04-15 17:09:12 +0300

    created primary button component

---
+++ src/components/ButtonPrimary.vue
@@ -0,0 +1,28 @@
+<template>
+  <button type="button" class="btn-primary">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="postcss" scoped>
+.btn-primary {
+  padding-left: 44px;
+  padding-right: 44px;
+  height: 44px;
+  background-color: var(--pc);
+  color: white;
+  font-weight: bold;
+  border-radius: 10px;
+  transition: 0.3s;
+  font-size: 16px;
+  user-select: none;
+
+  &:hover {
+    background-color: var(--sc);
+  }
+}
+</style>
