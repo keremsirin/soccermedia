d71c183996f6dbd7d7c88bd977060b0919c782f1
Kerem Şirin <30510378+keremsirin@users.noreply.github.com>
2021-05-03 21:08:55 +0300

    updated

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const tweets = require('./routes/api/tweets')
 
 app.use('/api/tweets', tweets)
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV == 'production') {
   app.use(express.static(__dirname + '/public/'))
   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
 }
