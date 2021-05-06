f6ebf2a01bcad43fb1aa440eaeeb06ded4cb18d5
Kerem Şirin <30510378+keremsirin@users.noreply.github.com>
2021-05-06 19:58:05 +0300

    updated

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use('/twittername/', twitterName)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(__dirname + '/public/'))
-  app.get('*', (req, res) => res.sendFile(__dirname + '/public/index.html'))
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
 }
 
 const port = process.env.PORT || 8081
