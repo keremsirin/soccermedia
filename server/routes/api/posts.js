47292ce6de63538b0f2d91f24c9d157ad78ca637
Kerem Şirin <30510378+keremsirin@users.noreply.github.com>
2021-06-16 02:00:04 +0300

    removed useless codes

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,27 +1,48 @@
 const express = require('express')
+const Instagram = require('instagram-web-api')
 const instagramPosts = require('instagram-posts')
+const axios = require('axios')
 
 const router = express.Router()
 
-let username = null
+let userName = null
 
 router.post('/instagramname', (req, res) => {
   console.log(req.body.userName)
   res.send(req.body.userName)
-  username = req.body.userName
+  userName = req.body.userName
 })
 
 router.get('/', (req, res) => {
+  // axios.get('https://www.instagram.com/cakirugurcan61/?__a=1').then((res) => {
+  //   console.log(res)
+  // })
+  const Instagram = require('instagram-web-api')
+  const username = 'soccer___media'
+  const password = 'xxx'
+
+  const client = new Instagram({ username, password })
+
   ;(async () => {
-    res.status(200).send(
-      await instagramPosts(
-        username,
-        (options = {
-          count: 5
-        })
-      )
-    )
+    await client.login()
+    const profile = await client.getUserByUsername({
+      username: userName
+    })
+    // console.log(profile)
+    res.status(200).send(profile)
+    // await client.logout()
   })()
+
+  // ;(async () => {
+  //   res.status(200).send(
+  //     await instagramPosts(
+  //       userName,
+  //       (options = {
+  //         count: 5
+  //       })
+  //     )
+  //   )
+  // })()
 })
 
 module.exports = router
