ca497bedfdc97c543111a41a1272238b34531240
Kerem Şirin <30510378+keremsirin@users.noreply.github.com>
2021-06-16 02:02:54 +0300

    updated component name

--- src/views/home/Posts.vue
+++ src/views/home/Posts.vue
@@ -2,21 +2,23 @@
   <div class="Posts">
     <CostumText class="Posts-title" tag="h1">Paylaşımlar</CostumText>
     <div class="Posts-container">
-      <Tweets />
-      <div></div>
+      <Twitter />
+      <!-- <Instagram /> -->
     </div>
   </div>
 </template>
 
 <script>
 import CostumText from '@/components/CostumText'
-import Tweets from '@/components/Twitter'
+import Twitter from '@/components/Twitter'
+// import Instagram from '@/components/Instagram'
 
 export default {
   name: 'Posts',
   components: {
     CostumText,
-    Tweets
+    Twitter
+    // Instagram
   },
   data() {
     return {}
@@ -41,9 +43,12 @@ export default {
 
   &-container {
     display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 28px;
-    /* justify-content: center; */
+    grid-template-columns: 1fr;
+    max-width: 600px;
+    /* grid-template-columns: 1fr 1fr; */
+    /* grid-gap: 28px; */
+    margin-left: auto;
+    margin-right: auto;
   }
 }
 </style>
